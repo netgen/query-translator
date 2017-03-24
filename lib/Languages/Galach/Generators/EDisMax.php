a35cce660e5e9105b758e51724a72917ce29004c
Petar Španja <petar@spanja.info>
2017-03-24 06:05:32 +0100

    Add EDisMax generator (visitors TBD)

---
+++ lib/Languages/Galach/Generators/EDisMax.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace QueryTranslator\Languages\Galach\Generators;
+
+use QueryTranslator\Languages\Galach\Generators\Native\Visitor;
+use QueryTranslator\Values\SyntaxTree;
+
+/**
+ * EDisMax generator generates query string in Solr's Extended DisMax query parser format.
+ *
+ * @link https://cwiki.apache.org/confluence/display/solr/The+Extended+DisMax+Query+Parser
+ */
+final class EDisMax
+{
+    /**
+     * @var \QueryTranslator\Languages\Galach\Generators\Native\Visitor
+     */
+    private $visitor;
+
+    public function __construct(Visitor $visitor)
+    {
+        $this->visitor = $visitor;
+    }
+
+    /**
+     * Generate query string in Solr's Extended DisMax format from the given $syntaxTree.
+     *
+     * @param \QueryTranslator\Values\SyntaxTree $syntaxTree
+     *
+     * @return mixed
+     */
+    public function generate(SyntaxTree $syntaxTree)
+    {
+        return $this->visitor->visit($syntaxTree->rootNode);
+    }
+}
