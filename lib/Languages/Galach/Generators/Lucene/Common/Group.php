6c699e684b0ef26c79f787e058756a0f09680ca8
Petar Španja <petar@spanja.info>
2017-04-26 22:39:11 +0200

    Simplify return value logic

--- lib/Languages/Galach/Generators/Lucene/Common/Group.php
+++ lib/Languages/Galach/Generators/Lucene/Common/Group.php
@@ -87,10 +87,6 @@ final class Group extends Visitor
             return $this->domainFieldMap[$token->domain];
         }
 
-        if ($this->defaultFieldName !== null) {
-            return $this->defaultFieldName;
-        }
-
-        return null;
+        return $this->defaultFieldName;
     }
 }
