df8ee4dd32d5a5cf10655c341684b225ba6a71ec
Petar Španja <petar@spanja.info>
2017-06-21 20:37:47 +0200

    Compare with null instead of using empty()

--- lib/Languages/Galach/Generators/Native/Phrase.php
+++ lib/Languages/Galach/Generators/Native/Phrase.php
@@ -34,7 +34,7 @@ final class Phrase extends Visitor
             );
         }
 
-        $domainPrefix = empty($token->domain) ? '' : "{$token->domain}:";
+        $domainPrefix = $token->domain === null ? '' : "{$token->domain}:";
         $phraseEscaped = preg_replace("/([\\{$token->quote}])/", '\\\\$1', $token->phrase);
 
         return "{$domainPrefix}{$token->quote}{$phraseEscaped}{$token->quote}";
