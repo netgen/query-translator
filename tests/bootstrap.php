5f24390cec25a4d12fc9e77e4021fd9bdfb96418
thePanz <thepanz@gmail.com>
2018-04-01 17:38:37 +0200

    Update phpunit integration

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$autoload = __DIR__ . '/../vendor/autoload.php';
-if (!file_exists($autoload)) {
-    throw new RuntimeException('Install dependencies using composer to run the test suite.');
-}
-
-require_once $autoload;
